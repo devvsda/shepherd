b723da5591575177f99426a984fe2e33d9f4bd7b
Hitesh Kumar Jhamb <hijhamb@Hiteshs-MacBook-Pro.local>
2018-01-11 13:27:46 +0530

    Added json response

--- shephard-core/src/main/java/com/devsda/platform/shephardcore/resources/HealthCheckReources.java
+++ shephard-core/src/main/java/com/devsda/platform/shephardcore/resources/HealthCheckReources.java
@@ -1,10 +1,15 @@
 package com.devsda.platform.shephardcore.resources;
 
 import com.devsda.platform.shephardcore.constants.ShephardConstants;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.net.InetAddress;
+import java.util.HashMap;
+import java.util.Map;
 
 @Path(ShephardConstants.Resources.HealthCheck)
 @Produces(MediaType.APPLICATION_JSON)
@@ -15,7 +20,16 @@ public class HealthCheckReources {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response healthCheck(@PathParam("echoText") String echoText) {
-        return Response.ok(echoText).build();
+    public Response healthCheck(@PathParam("echoText") String echoText) throws Exception {
+
+        Map<String, String> response = new HashMap<>();
+
+        response.put("echoString", echoText);
+        response.put("hostName", InetAddress.getLocalHost().getHostName());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String stringifyResponse = objectMapper.writeValueAsString(response);
+
+        return Response.ok(stringifyResponse).build();
     }
 }
