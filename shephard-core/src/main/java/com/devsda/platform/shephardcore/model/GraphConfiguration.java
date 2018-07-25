581b9d92f8134719228b2fa84b1cc10d4625a11f
devvsda <mail2jhamb@gmail.com>
2018-07-25 12:16:49 +0530

    Loader for workflow visualiser and workflow configuration  + corresponding UTs (#1)
    Loader for workflow visualiser and workflow configuration  + corresponding UTs

---
+++ shephard-core/src/main/java/com/devsda/platform/shephardcore/model/GraphConfiguration.java
@@ -0,0 +1,24 @@
+package com.devsda.platform.shephardcore.model;
+
+import java.util.List;
+
+public class GraphConfiguration {
+
+    private List<TeamConfiguration> teamConfigurations;
+
+
+    public List<TeamConfiguration> getTeamConfigurations() {
+        return teamConfigurations;
+    }
+
+    public void setTeamConfigurations(List<TeamConfiguration> teamConfigurations) {
+        this.teamConfigurations = teamConfigurations;
+    }
+
+    @Override
+    public String toString() {
+        return "GraphConfiguration{" +
+                "teamConfigurations=" + teamConfigurations +
+                '}';
+    }
+}
