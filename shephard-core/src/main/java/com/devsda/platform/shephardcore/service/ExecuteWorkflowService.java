5fa139cb44ab09ed1a6147ad439593f6639846ea
Hitesh Jhamb <mail2jhamb@gmail.com>
2018-08-14 20:37:21 +0530

    Integrated REST call to workflow owner

--- shephard-core/src/main/java/com/devsda/platform/shephardcore/service/ExecuteWorkflowService.java
+++ shephard-core/src/main/java/com/devsda/platform/shephardcore/service/ExecuteWorkflowService.java
@@ -18,12 +18,18 @@ public class ExecuteWorkflowService {
 
     private static final Logger log = LoggerFactory.getLogger(ExecuteWorkflowService.class);
 
+    /**
+     * This method helps to execute workflow.
+     * @param clientId Execute workflow for given client.
+     * @param endpointId Execute endpoint of above client.
+     * @param initialPayload Initial payload, which will be consumed by root node of workflow.
+     * @throws Exception
+     */
     public void executeWorkflow(String clientId, String endpointId, Map<String, Object> initialPayload) throws Exception {
 
 
         // TODO : Fetch required details from Database layer.
         // TODO : For now, I am using sample workflow , and its configurations.
-
 
         // Generate graph details.
         DAGGenerator dagGenerator = new DAGGenerator();
@@ -34,32 +40,39 @@ public class ExecuteWorkflowService {
         String workflowConfigurationPath = "./src/test/resources/workflow_configuration.json";
         GraphConfiguration graphConfiguration = JSONLoader.load(workflowConfigurationPath, GraphConfiguration.class);
 
-        log.info(String.format("Graph : %s", graph));
-        log.info(String.format("GraphConfiguration : %s", graphConfiguration));
+        log.info(String.format("Graph : %s. GraphConfiguration : %s", graph, graphConfiguration));
 
         // Graph
         executeGraph(graph, graphConfiguration);
 
     }
 
+
+    /**
+     * This method executes graph
+     * @param graph directed acyclic graph
+     * @param graphConfiguration configuration of all nodes under given graph
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
     private void executeGraph(Graph graph, GraphConfiguration graphConfiguration) throws InterruptedException, ExecutionException {
 
-        Map<String, NodeConfiguration> nodeNameToConfigurationMapping = GraphUtil.getNodeNameToConfigurationMapping(graphConfiguration);
-        Map<String, TeamConfiguration> teamNameToConfigurationMapping = GraphUtil.getTeamNameToConfigurationMapping(graphConfiguration);
+        Map<String, NodeConfiguration> nodeNameToNodeConfigurationMapping = GraphUtil.getNodeNameToNodeConfigurationMapping(graphConfiguration);
+        Map<String, TeamConfiguration> teamNameToTeamConfigurationMapping = GraphUtil.getTeamNameToTeamConfigurationMapping(graphConfiguration);
 
-        Map<String, Node> nameWiseNodeMapping = GraphUtil.nameWiseMapping(graph);
+        Map<String, Node> nodeNameToNodeMapping = GraphUtil.getNodeNameToNodeMapping(graph);
 
-        Map<String, List<String>> nodeToParentNodeMapping = GraphUtil.getNodeToParentNodeRelation(graph);
+        Map<String, List<String>> nodeToParentNodesMapping = GraphUtil.getNodeToParentNodesMapping(graph);
 
         String rootNode = GraphUtil.getRootNode(graph);
 
         ExecutorService executorService = Executors.newFixedThreadPool(10);
-        log.info(String.format("Created threadpool of count : %s", 10));
+        log.info(String.format("Created thread-pool of count : %s", 10));
 
-        NodeConfiguration rootNodeConfiguration = nodeNameToConfigurationMapping.get(rootNode);
-        ServerDetails rootNodeServerDetails = teamNameToConfigurationMapping.get(nameWiseNodeMapping.get(rootNode).getOwner()).getServerDetails();
+        NodeConfiguration rootNodeConfiguration = nodeNameToNodeConfigurationMapping.get(rootNode);
+        ServerDetails rootNodeServerDetails = teamNameToTeamConfigurationMapping.get(nodeNameToNodeMapping.get(rootNode).getOwner()).getServerDetails();
 
-        log.info(String.format("Submitting node : %s to threadpool for execution", rootNode));
+        log.info(String.format("Submitting node : %s to thread-pool for execution", rootNode));
         Future<NodeResponse> rootNodeFuture = executorService.submit(new NodeExecutor(rootNodeConfiguration, rootNodeServerDetails));
 
         Deque<Future<NodeResponse>> futureObjects = new LinkedList<>();
@@ -69,25 +82,24 @@ public class ExecuteWorkflowService {
 
             Future<NodeResponse> thisFutureObject = futureObjects.removeFirst();
 
-
             try {
-                NodeResponse nodeResponse = thisFutureObject.get(1000, TimeUnit.MILLISECONDS);
+                NodeResponse nodeResponse = thisFutureObject.get(1000l, TimeUnit.MILLISECONDS);
 
                 String nodeName = nodeResponse.getNodeName();
                 log.info(String.format("Node : %s successfully completed", nodeName));
 
-                nameWiseNodeMapping.get(nodeName).setNodeState(NodeState.COMPLETED);
+                nodeNameToNodeMapping.get(nodeName).setNodeState(NodeState.COMPLETED);
 
                 // TODO : This will use in CONDITIONAL workflow execution.
                 String clientResponse = nodeResponse.getClientResponse();
 
-                List<Connection> childrenConnections = nameWiseNodeMapping.get(nodeName).getConnections();
+                List<Connection> childrenConnections = nodeNameToNodeMapping.get(nodeName).getConnections();
 
                 if(childrenConnections == null) {
                     continue;
                 }
 
-                log.info(String.format("Number of children of node : %s is %s", nodeName, childrenConnections.size()));
+                log.debug(String.format("Number of children of node : %s is %s", nodeName, childrenConnections.size()));
 
                 for(Connection connection : childrenConnections) {
 
@@ -96,14 +108,16 @@ public class ExecuteWorkflowService {
 
                     String childNodeName = connection.getNodeName();
 
-                    Boolean isNodeReadyToExecute = ExecuteWorkflowServiceHelper.isNodeReadyToExecute(childNodeName, nodeToParentNodeMapping, nameWiseNodeMapping);
+                    Boolean isNodeReadyToExecute = ExecuteWorkflowServiceHelper.isNodeReadyToExecute(childNodeName, nodeToParentNodesMapping, nodeNameToNodeMapping);
 
                     if (Boolean.TRUE.equals(isNodeReadyToExecute)) {
-                        NodeConfiguration childNodeConfiguration = nodeNameToConfigurationMapping.get(childNodeName);
-                        ServerDetails childNodeServerDetails = teamNameToConfigurationMapping.get(nameWiseNodeMapping.get(childNodeName).getOwner()).getServerDetails();
+                        NodeConfiguration childNodeConfiguration = nodeNameToNodeConfigurationMapping.get(childNodeName);
+                        ServerDetails childNodeServerDetails = teamNameToTeamConfigurationMapping.get(nodeNameToNodeMapping.get(childNodeName).getOwner()).getServerDetails();
 
                         Future<NodeResponse> childNodeResponse = executorService.submit(new NodeExecutor(childNodeConfiguration, childNodeServerDetails));
                         futureObjects.addLast(childNodeResponse);
+                    } else {
+                        // Nothing to do.
                     }
 
                 }
